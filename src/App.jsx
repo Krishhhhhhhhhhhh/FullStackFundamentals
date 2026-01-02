0ee8111470a86dca44d5fe6a735bf8bec032cd07
krishna <pathakrishna2704@gmail.com>
2026-01-02 15:45:56 +0530

    feat: Initialize React application with App, CardWrapper, and TextComponent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
- 
-  const [title,setTitle]=useState("My name is ");
-
-  function updateTitle(){
-    setTitle("My name is "+ Math.random())
-  }
   return (
-  <>
-    <button onClick={updateTitle}>Update the title</button>
-    <Header title={title}></Header>
-    <Header title="head1"></Header>
-    <Header title="head2"></Header>
-  </>
+    <div>
+      <CardWrapper innerComponent={<TextComponent/>}/>
+    </div>
   )
+
+}
+function TextComponent() {
+  return <h1>TextComponent</h1>
+}
+function CardWrapper({innerComponent}) {
+  return<div style={{border:"2px solid black",padding:"10px",margin:"10px"}}>
+    {innerComponent}
+    </div>
 }
 
-function Header({title})
-{
-  return <div>
-    {title}
-  </div>
-}
+
 
 export default App
