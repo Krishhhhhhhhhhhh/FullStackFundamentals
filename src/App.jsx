88754f87291383a6ed1d650238ecc7df42bc5502
krishna <pathakrishna2704@gmail.com>
2026-01-09 18:29:15 +0530

    feat: add axios dependency and refactor App component to include counter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState, useMemo } from "react";
 
 function App() {
-  return (
-    <div style={{display:"flex"}}>
-     <Card>
-      <h2>Hi from h2</h2>
-     </Card>
-    </div>
-  )
-
-}
-function Card({children})
-{
-  return <div style={{border:"2px solid blue",padding:10,margin:10}}>
-    {children}
+  const [count,setCount]=useState(0)
+  return <div>
+    <button onClick={()=> {setCount(count+1)}}>Click me {count}</button>
   </div>
+  
 }
 
-export default App
+export default App;
