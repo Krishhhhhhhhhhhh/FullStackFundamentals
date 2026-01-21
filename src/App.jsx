e458f07da5b3a02858fbf9754bf4b64fce9f910b
krishna <pathakrishna2704@gmail.com>
2026-01-21 21:58:30 +0530

    feat: integrate Recoil for state management and add Count component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
 import { useContext,useState } from "react";
 import {CountContext} from "./context";
 import {Navigate} from "react-router-dom";
+import {useRecoilValue,RecoilRoot,useRecoilState} from "recoil"
+import {countAtom} from "./store/atoms/count.jsx"
 
 function App()
 {
-  count [count,setCount]=useState(0);
+ 
   return (
     <div>
-      
+      <RecoilRoot>
+
+      <Count/>
+
+      </RecoilRoot>
     </div>
   )
+}
+function Count()
+{
+  return <div>
+
+    <CountRenderer/>
+    <Buttons/>
+  </div>
+}
+
+function CountRenderer()
+{
+  const count=useRecoilValue(countAtom);
+  return <div>
+    <b>
+      {count}
+    </b>
+  </div>
+}
+function Buttons(){
+  const [count,setCount]=useRecoilState(countAtom);
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Increase</button>
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</button>
+  </div>
 }
